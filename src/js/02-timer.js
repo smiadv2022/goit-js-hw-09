8cd0dbcd91b2e2565aec28fd9e3d41a29bda194b
smiadv2022 <msorokina@spacemedia.com.ua>
2023-04-07 01:11:01 +0300

    1--3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,39 +1,34 @@
 // Описаний в документації
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-
-refInput= document.getElementById("datetime-picker");
-refStart= document.querySelector('button[data-start]');
-const  currentParam = {
+refInput = document.getElementById('datetime-picker');
+refStart = document.querySelector('button[data-start]');
+const currentParam = {
   days: document.querySelector('span[data-days]'),
   hours: document.querySelector('span[data-hours]'),
   minutes: document.querySelector('span[data-minutes]'),
-  seconds:  document.querySelector('span[data-seconds]')
-}
+  seconds: document.querySelector('span[data-seconds]'),
+};
 
-console.log(refStart);
-
-const startTimer = refStart.addEventListener("click",  onStartTimer);
+const startTimer = refStart.addEventListener('click', onStartTimer);
 
 let selData = new Date();
 
 refStart.disabled = true;
-let diff1=0;
+let diff1 = 0;
 
 function controlDisable() {
   if (selData > new Date()) {
     refStart.disabled = false;
-    console.log ("selData", selData)
   } else {
     refStart.disabled = true;
 
     Notiflix.Notify.failure('Please choose a date in the future');
   }
 }
-
 
 const options = {
   enableTime: true,
@@ -42,31 +37,11 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selData = selectedDates[0];
-    console.log("opt",selectedDates[0]);
-    // console.log("selectopt",selectedDates[0].setTime(selectedDates[0].getTime()) );
-    //  diff1 = userDate.selectedDates[0] - userDate.now;
-    // console.log ("diff1 opt", diff1);
-    controlDisable ();
+
+    controlDisable();
   },
 };
-let userDate=flatpickr(refInput, options);
-
-// const idStart =setInterval (onStartTimer, 1000);
-// let now = userDate.now;
-// let  selec =userDate.selectedDates[0];
-// let diff = userDate.selectedDates[0] - userDate.now;
-// //  controlDisable();
-//  console.log("sdiff", diff);
-//  console.log("sdiff1 telo", diff1);
-// const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-// const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-// const minutes = Math.floor((diff / (1000 * 60)) % 60);
-// const seconds = Math.floor((diff / 1000) % 60);
-// console.log("now",now.setTime(now.getTime()) );
-// console.log("select",selec.setTime(selec.getTime()) );
-// const nnn = new Date (selec)
-// console.log("select n d", new Date (selec) );
-// function currentDiff (par) { new Date() - par};
+let userDate = flatpickr(refInput, options);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -87,34 +62,24 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(diff1)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
 function onStartTimer() {
   refStart.disabled = true;
 
   const idInterval = setInterval(() => {
     const a = convertMs(selData.getTime() - new Date().getTime());
-    if ((selData.getTime() - new Date().getTime()) <= 0) {
+    if (selData.getTime() - new Date().getTime() <= 0) {
       clearInterval(idInterval);
-      console.log(" finishhhhhhhhhhhhhhhh")
-      return;}
-    console.log('seldata', selData.getTime(), selData);
-    console.log('new data', new Date().getTime(), new Date());
-    console.log(a);
+
+      return;
+    }
 
     Object.entries(a).forEach(([name, value]) => {
-      // console.log("bbbb", name,value);
       currentParam.days.textContent = addLeadingZero3(a.days);
       currentParam.hours.textContent = addLeadingZero2(a.hours);
       currentParam.minutes.textContent = addLeadingZero2(a.minutes);
       currentParam.seconds.textContent = addLeadingZero2(a.seconds);
     });
-     
   }, 1000);
-
- 
 }
 function addLeadingZero2(number) {
   return String(number).padStart(2, 0);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,53 +1,39 @@
- import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
-const refForm= document.querySelector  (".form");
-// const refDelay= document.getElementById('delay');
-// const refBtn= document.querySelector('button');
-// const refStep = document.querySelector('[name="step"]');
-// const refAmount = document.querySelector('[name="amount"]');
-// console.log (refForm);
-// console.log ("nn", refDelay );
-// console.log ("bt", refBtn);
+const refForm = document.querySelector('.form');
 
-
- refForm.addEventListener('submit', onStart);
-//  refAmount.addEventListener('change', onAmount);
-// refDelay.addEventListener('change', onDelay);
-// refStep.addEventListener('change',onStep);
-
-// function onDelay ()  { console.log("del",refDelay.value); return refDelay.value;};
-// function onAmount ()  { console.log("am",refAmount.value);};
-// function onStep ()  { 
-//   console.log("step",refStep.value);
-// };
+refForm.addEventListener('submit', onStart);
 
 function onStart(event) {
   let iPosition = 0;
-  let iDelay =Number(event.currentTarget.delay.value);
-  
- iDelay = Number(event.currentTarget.delay.value)*1;
+  let iDelay = Number(event.currentTarget.delay.value);
+
+  iDelay = Number(event.currentTarget.delay.value) * 1;
   event.preventDefault();
-  // console.log('11111111111', event.currentTarget.amount.value, i);
-  // console.log("st",iDelay);
-  // console.log("stelements",onDelay());
+
   while (iPosition < event.currentTarget.amount.value) {
     iPosition++;
-      console.log("del-??/", iDelay);
-    console.log(iPosition);
+
     createPromise(iPosition, iDelay);
     iDelay += Number(event.currentTarget.step.value);
-  
-    
   }
 }
 
-
-
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise.then(
+    (result) =>
+      Notiflix.Notify.success(result),
+    (err) =>
+      Notiflix.Notify.failure(err)
+  );
 }
